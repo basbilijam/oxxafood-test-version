0a3740b00998abb3e721fc5f6b16384d97908bef
basbilijam <basbilijam@gmail.com>
2017-07-03 20:31:40 +0200

    test version

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
 $(document).ready(function(){
-//   $(window).resize(function(){
-//
-//     if($(window).width()<768){
-//       $('.navbar.navbar-inverse').css('background-color','green');
-//     }
-//     else
-//     {
-//       $('.navbar-nav').css('background-color','#222');
-//     }
-//   });
-// });
+
 // navbar active item
  $(".navbar-nav a").on("click", function(){
   $(".navbar-nav").find(".active").removeClass("active");
   $(this).parent().addClass("active");
-});
+  });
 
 // collapse navbar after link is clicked
-// $(".navbar-nav a").click(function(event) {
-//    if (!$(this).parent().hasClass('dropdown'))
-//        $(".navbar-collapse").collapse('hide');
-//   });
+$(".navbar-nav a").click(function(event) {
+   if (!$(this).parent().hasClass('dropdown'))
+       $(".navbar-collapse").collapse('hide');
+  });
 });
